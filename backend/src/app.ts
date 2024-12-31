0fb17a171d2e866d5419376a5da89d67a7c51ef6
Imadh-Ifham <imadhifham@gmail.com>
2024-12-31 14:25:06 +0530

    feat: Initialized CRUD setup for Blueprint management

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Express, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import gameRoutes from "./routes/game.routes";
+import layoutRoutes from "./routes/blueprint.routes";
 
 const app: Express = express();
 
@@ -12,7 +13,8 @@ app.use(express.urlencoded({ extended: true }));
 
 // Routes
 app.use("/api/users", userRoutes);
-app.use("/api/games", gameRoutes); // Mount the game router
+app.use("/api/games", gameRoutes);
+app.use("/api/layouts", layoutRoutes);
 
 // Error handling
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
