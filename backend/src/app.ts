d9c8651ab18c7d5120e428bcd097bc0a6219d336
Senuthii Amarasinghe <asenuthisahansa@gmail.com>
2025-01-02 09:42:38 +0530

    event teams crud
    completed event teams crud

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -2,7 +2,8 @@ import express, { Express, Request, Response, NextFunction } from "express";
 import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import gameRoutes from "./routes/game.routes";
-import eventRoutes from "./routes/event.routes";
+import eventRoutes from "./routes/event.routes/event.routes";
+import teamRoutes from "./routes/event.routes/team.routes";
 
 const app: Express = express();
 
@@ -14,7 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/users", userRoutes);
 app.use("/api/games", gameRoutes); // Mount the game router
-app.use("/api/events", eventRoutes);
+app.use("/api/events", eventRoutes); //events routes
+app.use("/api/teams", teamRoutes); // teams in events routes
 
 // Error handling
 app.use((err: any, req: Request, res: Response, next: NextFunction) => {
