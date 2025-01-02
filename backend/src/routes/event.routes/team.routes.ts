9ae12f1d9b5c420c6a83849206154e0a5feaf71a
Senuthii Amarasinghe <asenuthisahansa@gmail.com>
2025-01-02 13:37:43 +0530

    added routes registerUserToTeam, removeUserFromTeam

--- backend/src/routes/event.routes/team.routes.ts
+++ backend/src/routes/event.routes/team.routes.ts
@@ -1,13 +1,25 @@
 import express, { Router } from "express";
-import { createTeam, deleteTeam, getAllTeams, getTeamById, updateTeam } from "../../controllers/event.controller/team.controller";
+import { 
+    createTeam, 
+    deleteTeam, 
+    getAllTeams, 
+    getTeamById, 
+    updateTeam, 
+    getEventAllTeams,
+    registerUserToTeam,
+    removeUserFromTeam
+ } from "../../controllers/event.controller/team.controller";
 
 const router: Router = express.Router();
 
 //team routes
 router.post('/', createTeam); //create team
-router.get('/:id', getAllTeams); //get all teams
+router.get('/', getAllTeams); //get all teams
+router.get('/:id', getEventAllTeams); //get event and teams
 router.get('/:id', getTeamById); //get team by id
 router.put('/:id', updateTeam); //update team
 router.delete('/:id', deleteTeam); //delete team
+router.put('/:teamId/register', registerUserToTeam); // Register user to team
+router.put('/:teamId/remove', removeUserFromTeam); // Remove user from team
 
 export default router;
