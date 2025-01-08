f3f2716f5319e357e2d97641fc5a286ee74a89df
Hariswara <harrishphotos@gmail.com>
2025-01-08 21:30:54 +0530

    fixed browser routes

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { BrowserRouter } from "react-router-dom";
 import App from "./App.tsx";
 
 const rootElement = document.getElementById("root");
@@ -9,9 +8,7 @@ if (rootElement) {
   const root = createRoot(rootElement);
   root.render(
     <StrictMode>
-      <BrowserRouter>
-        <App />
-      </BrowserRouter>
+      <App />
     </StrictMode>
   );
 }
