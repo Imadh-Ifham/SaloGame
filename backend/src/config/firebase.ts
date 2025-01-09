2ceea9742cf3a1cebfd19c629b25a9c596ded499
ApekshaRathgalle <168087949+ApekshaRathgalle@users.noreply.github.com>
2025-01-09 07:14:49 +0530

    firebase project created

---
+++ backend/src/config/firebase.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBh_xH-jaJ3gFkwdfhy_prKR-i3I2mq2vU",
+    authDomain: "authentication-2a2fd.firebaseapp.com",
+    projectId: "authentication-2a2fd",
+    storageBucket: "authentication-2a2fd.firebasestorage.app",
+    messagingSenderId: "455649401557",
+    appId: "1:455649401557:web:a5707ec47f60bdb459205c",
+    measurementId: "G-CBZYH2X7X9"
+  };
+
+  const app = initializeApp(firebaseConfig);
